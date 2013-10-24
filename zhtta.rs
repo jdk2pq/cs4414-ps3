df6512e31d323ca1af879b7d618346b3e8e7c7d6
jlw8ke <jlw8ke@live.com>
2013-10-24 05:04:43 -0400

    wahoo scheduling

--- zhtta.rs
+++ zhtta.rs
@@ -14,6 +14,7 @@
 
 extern mod extra;
 
+
 use std::rt::io::*;
 use std::rt::io::net::ip::SocketAddr;
 use std::io::println;
@@ -22,8 +23,10 @@ use std::{os, str, io};
 use extra::arc;
 use std::comm::*;
 
+
 static PORT:    int = 4414;
 static IP: &'static str = "127.0.0.1";
+
 
 struct sched_msg {
     stream: Option<std::rt::io::net::tcp::TcpStream>,
@@ -41,13 +44,18 @@ fn main() {
 
     let visitor_count: uint = 0;
     let shared_visitor_count = arc::RWArc::new(visitor_count);
-    
+
+    //Wahoo Scheduling
+    let wahoo_index:uint = 0;
+    let shared_wahoo_index = arc::RWArc::new(wahoo_index);
+    let local_wahoo_index = shared_wahoo_index.clone();
+
 
     // dequeue file requests, and send responses.
     // FIFO
     do spawn {
         let (sm_port, sm_chan) = stream();
-        
+
         // a task for sending responses.
         do spawn {
             loop {
@@ -76,6 +84,14 @@ fn main() {
                     let tf = tf_opt.unwrap();
                     println(fmt!("shift from queue, size: %ud", (*vec).len()));
                     sm_chan.send(tf); // send the request to send-response-task to serve.
+
+                    //Decrement wahoo index
+                    do local_wahoo_index.write |count| {
+                        if ((*count) > 0 ) {
+                            (*count) -= 1;
+                            println(fmt!("wahoo index decremented to %u", (*count)));
+                        }
+                    }                  
                 }
             }
         }
@@ -92,22 +108,41 @@ fn main() {
     let mut acceptor = socket.listen().unwrap();
     
     for stream in acceptor.incoming() {
-        let stream = Cell::new(stream);
-        let local_visitor_count = shared_visitor_count.clone();
+        //Getting the ip address of the socket
+        let mut stream = stream;
+        let mut incoming_ip: ~str = ~"";
+        match stream {
+            Some(ref mut theTcpStream) => {
+                match theTcpStream.peer_name() {
+                    Some(peername) => {
+                        incoming_ip = peername.to_str();
+                    }
+                    None => ()                    
+                }
+            }
+            None => ()
+        }
 
-        
+        //Cells to pass in the strem and ip address to the closure function
+        let stream = Cell::new(stream);
+        let incoming_ip = Cell::new(incoming_ip);
+
+        let local_visitor_count = shared_visitor_count.clone();
+        let local_wahoo_index = shared_wahoo_index.clone();
         // Start a new task to handle the each connection
         let child_chan = chan.clone();
         let child_add_vec = add_vec.clone();
 
         do spawn {
-            let mut vis_count = 0;
+            let mut visitor_count = 0;
             do local_visitor_count.write |count| {
                 (*count) += 1;
-                vis_count = (*count);
+                visitor_count = (*count);
             }
             
             let mut stream = stream.take();
+            let mut incoming_ip = incoming_ip.take();
+
             let mut buf = [0, ..500];
             stream.read(buf);
             let request_str = str::from_utf8(buf);
@@ -130,7 +165,7 @@ fn main() {
                          <body>
                          <h1>Greetings, Krusty!</h1>
                          <h2>Visitor count: %u</h2>
-                         </body></html>\r\n", vis_count);
+                         </body></html>\r\n", visitor_count);
 
                     stream.write(response.as_bytes());
                 }
@@ -139,11 +174,29 @@ fn main() {
                     let msg: sched_msg = sched_msg{stream: stream, filepath: file_path.clone()};
                     let (sm_port, sm_chan) = std::comm::stream();
                     sm_chan.send(msg);
-                    
+
+                    incoming_ip = ~"128.143.";
+                    let mut wahoo_index = 0;                    
+                    if checkWahoo(incoming_ip)
+                    {
+                        println("Found a Charlottesville address");
+                        do local_wahoo_index.write |count| {
+                            (*count) += 1;
+                            wahoo_index = (*count);
+                        }
+                        println(fmt!("Wahoo requests end at index: %u", wahoo_index));
+                    }
+                                        
                     do child_add_vec.write |vec| {
                         let msg = sm_port.recv();
-                        (*vec).push(msg); // enqueue new request.
-                        println("add to queue");
+                        if wahoo_index > 0 {
+                            (*vec).insert(wahoo_index-1, msg); //place at back of Charlottesville queue
+                            println("inserted at back of Charlottesville queue");
+                        }
+                        else {
+                            (*vec).push(msg); // enqueue new request.
+                            println("add to queue");
+                        }
                     }
                     child_chan.send(""); //notify the new arriving request.
                     println(fmt!("get file request: %?", file_path));
@@ -152,4 +205,16 @@ fn main() {
             println!("connection terminates")
         }
     }
+}
+
+fn checkWahoo(ip: ~str) -> bool {
+    let WAHOO_IP = ~["128.143.", "137.54."]; 
+    println(fmt!("ip: %s", ip));
+       
+    for matcher in WAHOO_IP.iter() {
+        if ip.contains(*matcher) {
+            return true;
+        }
+    }
+    return false;
 }
