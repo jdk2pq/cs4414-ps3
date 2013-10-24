96f00592f04036f7002429d2c4398726ba7bcf04
Jake Kenneally <jdk2pq@virginia.edu>
2013-10-24 01:47:51 -0400

    server side gashing! a few things to touch up in the comments, but it's actually working woooooo

--- zhtta.rs
+++ zhtta.rs
@@ -18,10 +18,12 @@ use std::rt::io::*;
 use std::rt::io::net::ip::SocketAddr;
 use std::io::println;
 use std::cell::Cell;
-use std::{os, str, io};
+use std::{os, str, io, run};
 use extra::arc;
 use std::comm::*;
-// use gash;
+use gash::handle_cmdline;
+
+mod gash;
 
 static PORT:    int = 4414;
 static IP: &'static str = "127.0.0.1";
@@ -29,6 +31,28 @@ static IP: &'static str = "127.0.0.1";
 struct sched_msg {
     stream: Option<std::rt::io::net::tcp::TcpStream>,
     filepath: ~std::path::PosixPath
+}
+
+//allows for server-side gashing replacement of '<--!#exec cmd="{gash-command}" -->'
+//admittedly lousy implementation, and only replaces the first instance of a command at the moment
+//but i've been working on it for hours, and it does something, and i'm happy
+fn exec_command(file_data: &str) -> ~str {
+    let mut build_result: ~[~str] = ~[~""];
+    // while (file_data.contains("<--!#exec cmd=")) { //hopefully i can get this working to replace every instance of a command
+        println("replacing SSI gashing!");
+        let index = file_data.find_str("<--!#exec cmd=").unwrap();
+        build_result.push(file_data.slice_to(index).to_owned());
+        let next_part = file_data.slice_from(index);
+        let command_stmt = next_part.slice_to(next_part.find_str("\" -->").unwrap()+5);
+        let mut cmd = command_stmt.replace("<--!#exec cmd=\"", "");
+        cmd = cmd.replace("\" -->", "");
+        // println(fmt!("command is %?", cmd)); //for testing
+        let x: Option<run::ProcessOutput> = handle_cmdline(cmd);
+        // println(fmt!("test %?", str::from_utf8(handle_cmdline("date").unwrap().output))); //for testing 
+        build_result.push(str::from_utf8(x.unwrap().output));
+        build_result.push(next_part.slice_from(next_part.find_str("-->").unwrap()+3).to_owned());
+    // }
+    return build_result.concat();
 }
 
 fn main() {
@@ -62,14 +86,24 @@ fn main() {
                 //Serve WahooFirst...first
                 while (sm_port_char.peek()) {
                     let mut tf: sched_msg = sm_port_char.recv(); // wait for the dequeued request to handle
-                    match io::read_whole_file(tf.filepath) { // killed if file size is larger than memory size.
+                    match io::read_whole_file_str(tf.filepath) { // killed if file size is larger than memory size.
                         Ok(file_data) => {
                             println(fmt!("begin serving file from wa-queue [%?]", tf.filepath));
                             println(fmt!("file size is %?", tf.filepath.get_size().unwrap()));
-                            // A web server should always reply a HTTP header for any legal HTTP request.
-                            tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream; charset=UTF-8\r\n\r\n".as_bytes());
-                            tf.stream.write(file_data);
-                            println(fmt!("finish file from wa-queue [%?]", tf.filepath));
+                            if (file_data.contains("<--!#exec cmd=")) {
+                                let file_data_str = exec_command(file_data);
+                                // A web server should always reply a HTTP header for any legal HTTP request.
+                                tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream; charset=UTF-8\r\n\r\n".as_bytes());
+                                println(fmt!("file contains: %?", file_data_str));
+                                tf.stream.write(file_data_str.as_bytes());
+                                println(fmt!("finish file [%?]", tf.filepath));
+                            } else {
+                                // A web server should always reply a HTTP header for any legal HTTP request.
+                                tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream; charset=UTF-8\r\n\r\n".as_bytes());
+                                println(fmt!("file contains: %?", file_data));
+                                tf.stream.write(file_data.as_bytes());
+                                println(fmt!("finish file [%?]", tf.filepath));
+                            }
                         }
                         Err(err) => {
                             println(err);
@@ -77,14 +111,24 @@ fn main() {
                     }
                 }
                 let mut tf: sched_msg = sm_port.recv(); // wait for the dequeued request to handle
-                match io::read_whole_file(tf.filepath) { // killed if file size is larger than memory size.
+                match io::read_whole_file_str(tf.filepath) { // killed if file size is larger than memory size.
                     Ok(file_data) => {
                         println(fmt!("begin serving file [%?]", tf.filepath));
                         println(fmt!("file size is %?", tf.filepath.get_size().unwrap()));
-                        // A web server should always reply a HTTP header for any legal HTTP request.
-                        tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream; charset=UTF-8\r\n\r\n".as_bytes());
-                        tf.stream.write(file_data);
-                        println(fmt!("finish file [%?]", tf.filepath));
+                        if (file_data.contains("<--!#exec cmd=")) {
+                            let file_data_str = exec_command(file_data);
+                            // A web server should always reply a HTTP header for any legal HTTP request.
+                            tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream; charset=UTF-8\r\n\r\n".as_bytes());
+                            println(fmt!("file contains: %?", file_data_str));
+                            tf.stream.write(file_data_str.as_bytes());
+                            println(fmt!("finish file [%?]", tf.filepath));
+                        } else {
+                            // A web server should always reply a HTTP header for any legal HTTP request.
+                            tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream; charset=UTF-8\r\n\r\n".as_bytes());
+                            println(fmt!("file contains: %?", file_data));
+                            tf.stream.write(file_data.as_bytes());
+                            println(fmt!("finish file [%?]", tf.filepath));
+                        }
                     }
                     Err(err) => {
                         println(err);
