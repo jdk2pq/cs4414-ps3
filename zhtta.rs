c13a659045e546391514865e3be337359c077c73
jlw8ke <jlw8ke@live.com>
2013-10-23 23:01:40 -0400

    problem 2

--- zhtta.rs
+++ zhtta.rs
@@ -15,7 +15,7 @@
 extern mod extra;
 
 use std::rt::io::*;
-use std::rt::io::net::ip::{SocketAddr, Ipv4Addr};
+use std::rt::io::net::ip::{SocketAddr};
 use std::io::println;
 use std::cell::Cell;
 use std::{os, str, io};
@@ -23,7 +23,7 @@ use extra::arc;
 use std::comm::*;
 
 static PORT: int = 4414;
-static IPV4_LOOPBACK: &'static str = "127.0.0.1";
+static IP: &'static str = "0.0.0.0";
 
 struct sched_msg {
     stream: Option<std::rt::io::net::tcp::TcpStream>,
@@ -31,6 +31,7 @@ struct sched_msg {
 }
 
 fn main() {
+
     let req_vec: ~[sched_msg] = ~[];
     let shared_req_vec = arc::RWArc::new(req_vec);
     let add_vec = shared_req_vec.clone();
@@ -38,12 +39,27 @@ fn main() {
     let (port, chan) = stream();
     let chan = SharedChan::new(chan);
 
+    //Charlottesville Channel
+    let req_vec_wahoo: ~[sched_msg] = ~[];
+    let shared_req_vec_wahoo = arc::RWArc::new(req_vec_wahoo);
+    let add_vec_wahoo = shared_req_vec_wahoo.clone();
+    let take_vec_wahoo = shared_req_vec_wahoo.clone();
+    let (port_wahoo, chan_wahoo) = stream();
+    let chan_wahoo = SharedChan::new(chan_wahoo);
+
     let visitor_count: uint = 0;
     let shared_vis_count = arc::RWArc::new(visitor_count);
     
     // add file requests into queue.
     do spawn {
         while(true) {
+            do add_vec_wahoo.write |vec| {
+                if (port_wahoo.peek()) {
+                    let tf:sched_msg = port.recv();
+                    (*vec).push(tf);
+                    println(fmt!("add to wa-queue, size: %ud", (*vec).len()));
+                }
+            }
             do add_vec.write |vec| {
                 let tf:sched_msg = port.recv();
                 (*vec).push(tf);
@@ -55,35 +71,91 @@ fn main() {
     // take file requests from queue, and send a response.
     do spawn {
         while(true) {
+            do take_vec_wahoo.write |vec| {
+                if ((*vec).len() > 0) {
+                    let tf_opt: Option<sched_msg> = (*vec).shift_opt();
+                    let mut tf = tf_opt.unwrap();
+                    println(fmt!("shift from wa-queue, size: %ud", (*vec).len()));
+
+                    match io::read_whole_file(tf.filepath) { // killed if file size is larger than memory size.
+                        Ok(file_data) => {
+                            println(fmt!("begin serving file [%?]", tf.filepath));
+                            tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream; charset=UTF-8\r\n\r\n".as_bytes());
+                            tf.stream.write(file_data);
+                            println(fmt!("finish file [%?]", tf.filepath));
+                        }
+                        Err(err) => {
+                            println(err);
+                        }
+                    } 
+                }
+            }
             do take_vec.write |vec| {
-                let mut tf = (*vec).pop();
-                
-                match io::read_whole_file(tf.filepath) {
-                    Ok(file_data) => {
-                        tf.stream.write(file_data);
-                    }
-                    Err(err) => {
-                        println(err);
+                if ((*vec).len() > 0) {
+                    // FILO didn't make sense in service scheduling, so we modify it as FIFO by using shift_opt() rather than pop().
+                    let tf_opt: Option<sched_msg> = (*vec).shift_opt();
+                    let mut tf = tf_opt.unwrap();
+                    println(fmt!("shift from queue, size: %ud", (*vec).len()));
+
+                    match io::read_whole_file(tf.filepath) { // killed if file size is larger than memory size.
+                        Ok(file_data) => {
+                            println(fmt!("begin serving file [%?]", tf.filepath));
+                            // A web server should always reply a HTTP header for any legal HTTP request.
+                            tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream; charset=UTF-8\r\n\r\n".as_bytes());
+                            tf.stream.write(file_data);
+                            println(fmt!("finish file [%?]", tf.filepath));
+                        }
+                        Err(err) => {
+                            println(err);
+                        }
                     }
                 }
             }
         }
     }
     
-    let socket = net::tcp::TcpListener::bind(SocketAddr {ip: Ipv4Addr(127,0,0,1), port: PORT as u16});
+    let ip = match FromStr::from_str(IP) { 
+        Some(ip) => ip, 
+        None => { 
+            println(fmt!("Error: Invalid IP address <%s>", IP));
+            return;
+        },
+    };
+    println(fmt!("ip address: %?", ip));
+    let socket = net::tcp::TcpListener::bind(SocketAddr {ip: ip, port: PORT as u16});
     
     println(fmt!("Listening on tcp port %d ...", PORT));
     let mut acceptor = socket.listen().unwrap();
     
     // we can limit the incoming connection count.
     //for stream in acceptor.incoming().take(10 as uint) {
-    for stream in acceptor.incoming() {
+        for stream in acceptor.incoming() {
+            let mut stream = stream;
+            let mut peer_name: ~str = ~"";
+            match stream {
+                Some(ref mut s) => {
+                    match s.peer_name() {
+                        Some(peername) => {
+                        //println( fmt!("Peer address: %s \n", peername.to_str() ));
+                        peer_name = peername.to_str();
+                    }
+                    None => ()
+                }
+            }
+            None => ()
+        }
+
         let stream = Cell::new(stream);
+        let peer_name = Cell::new(peer_name);
+
         let local_vis_count = shared_vis_count.clone();
         // Start a new task to handle the connection
         let child_chan = chan.clone();
+        let child_chan_wahoo = chan_wahoo.clone();
         do spawn {
             let mut vis_count = 0;
+            let peer_name = peer_name.take();
+
             do local_vis_count.write |count| {
                 (*count) = (*count)+1;
                 vis_count = (*count);
@@ -104,24 +176,30 @@ fn main() {
                     println(fmt!("Request received:\n%s", request_str));
                     let response: ~str = fmt!(
                         "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
-<doctype !html><html><head><title>Hello, Rust!</title>
-<style>body { background-color: #111; color: #FFEEAA }
-h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
-h2 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm green}
-</style></head>
-<body>
-<h1>Greetings, Krusty!</h1>
-<h2>Visitor count: %u</h2>
-</body></html>\r\n", vis_count);
+                        <doctype !html><html><head><title>Hello, Rust!</title>
+                        <style>body { background-color: #111; color: #FFEEAA }
+                        h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
+                        h2 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm green}
+                        </style></head>
+                        <body>
+                        <h1>Greetings, Krusty!</h1>
+                        <h2>Visitor count: %u</h2>
+                        </body></html>\r\n", vis_count);
 
                     stream.write(response.as_bytes());
                 }
                 else {
                     // may do scheduling here
-                    let msg: sched_msg = sched_msg{stream: stream, filepath: file_path.clone()};
-                    child_chan.send(msg);
-                    
-                    println(fmt!("get file request: %?", file_path));
+                    if (peer_name.starts_with("128.143.") || peer_name.starts_with("137.54.") || peer_name.starts_with("127.")) {
+                        println(fmt!("peer_name fits into Charlottesville. Going into Wa-queue"));
+                        let msg: sched_msg = sched_msg{stream: stream, filepath: file_path.clone()};
+                        child_chan_wahoo.send(msg);
+                    } else {
+                        println(fmt!("peer_name: %?", peer_name));
+                        let msg: sched_msg = sched_msg{stream: stream, filepath: file_path.clone()};
+                        child_chan.send(msg);
+                        println(fmt!("get file request: %?", file_path));
+                    }
                 }
             }
             println!("connection terminates")
